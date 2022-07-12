282fd539705780b99f9bcd1d9136d18035afca50
Jonathan Britton <jbritttt29@hotmail.com>
2022-07-12 11:04:11 +0100

    Refactor tests to use each

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,17 +1,20 @@
-const getLargest = require('./getLargest')
 
-test('getLargest is a function', () =>{
 
-expect (typeof getLargest).toBe('function')
+const getLargest = require("./getLargest");
 
+describe("getLargest", () => {
+  test("getLargest is a function", () => {
+    expect(typeof getLargest).toBe("function");
+  })
+
+
+  describe("It handles normal inputs successfully ", () => {
+
+each([[1,2,3],3]).test('',(arr, expected) =>{
+
+    expect(getLargest)(arr).toEqual(expected)
 })
 
 
-test('getLargest returns the largest value', () =>{
-
-
-const result = getLargest(1,2,3)
-
-expect(result).toEqual(3);
-
-})
+  })
+});
